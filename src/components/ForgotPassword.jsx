410d00b2e5e2f3e9eab7e2802dae7100c286e87d
Hemamalini-Kamaraj <hemamalini.ahkm@gmail.com>
2023-08-19 15:24:30 +0530

    done

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import baseUrl from "../api/api";
 
 function ForgotPassword() {
+
+  let [email, setEmail] = useState("");
+  const navigate = useNavigate("");
+
+  const handleForgotPassword = async (e) => {
+    e.preventDefault();
+    const forgotEmail = email;
+// console.log(email)
+    let users = await baseUrl.get("/user");
+    users = users.data;
+    users = users.find((user) => user.email === forgotEmail);
+    if (users) {
+      try {
+        baseUrl.put("/user/forgot-password", { email: forgotEmail });
+        setEmail("");
+        alert(
+          "Mail has been sent to reset the password.Kindly check your email!"
+        );
+        navigate("/");
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("User does not exist. Kindly register!");
+    }
+  };
+
   return (
     <Container fluid className="login">
         <Row className="vh-100 d-flex justify-content-center align-items-center">
@@ -14,13 +42,13 @@ function ForgotPassword() {
                   <h1 className="fw-bold  text-uppercase text-center" style={{color:"#6f4db0"}}><img src={logo} style={{width:"40px", marginTop:"-9px"}}></img> Fitness Logger</h1>
                   <p className="mb-4 fs-6 fw-bold text-center" style={{color:"#6f4db0"}}>CHANGE YOUR PASSWORD HERE..!</p>
                   <div className="mb-3">
-                    <Form>   
+                    <Form onSubmit={handleForgotPassword}>   
                                  
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="text-center">
                           Email address
                         </Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                       </Form.Group>
                                       
                       <div className="d-grid">
--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import baseUrl from "../api/api";
+
 
 function ResetPassword() {
+  let [password, setPassword] = useState("");
+  let [cPassword, setcPassword] = useState("");
+  let [resetToken, setResetToken] = useState("");
+
+  let { id } = useParams();
+  useEffect(() => {
+    setResetToken(id);
+  }, [id]);
+
+  const handleReset = async (e) => {
+    e.preventDefault();
+
+    let users = await baseUrl.get("/user");
+    users = users.data;
+    users = users.find((user) => user.resetToken === resetToken);
+    if (users) {
+      if (password === cPassword) {
+        baseUrl.patch(`/user/reset-password/${resetToken}`, {
+          password: password,
+        });
+        setPassword("");
+        setcPassword("");
+        alert("Password changed Successfully!");
+      } else {
+        alert("Password not matching");
+      }
+    } else {
+      alert("Reset password link has been expired!");
+    }
+  };
   return (
     <Container fluid className="login">
         <Row className="vh-100 d-flex justify-content-center align-items-center">
@@ -14,16 +46,16 @@ function ResetPassword() {
                   <h1 className="fw-bold  text-uppercase text-center" style={{color:"#6f4db0"}}><img src={logo} style={{width:"40px", marginTop:"-9px"}}></img> Fitness Logger</h1>
                   <p className="mb-4 fs-6 fw-bold text-center" style={{color:"#6f4db0"}}>RESET YOUR PASSWORD HERE..!</p>
                   <div className="mb-3">
-                    <Form>   
+                    <Form onSubmit={handleReset}>   
                                  
                       <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Enter Password" />
                       </Form.Group>
                      
-                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter Password" />
+                        <Form.Control type="password" placeholder="ReEnter Password" />
                       </Form.Group>
                                       
                       <div className="d-grid">
